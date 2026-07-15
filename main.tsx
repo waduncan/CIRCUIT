9ff07c96ce46bec43179cd6d854d12dbd10e8316
Will Duncan <waduncan21@gmail.com>
2026-07-15 09:37:15 -0500

    lil fix

--- main.tsx
+++ main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import DiagramApp from "./app/DiagramApp";
+//@ts-ignore
 import "./app/globals.css";
 
 const root = document.getElementById("root");
@@ -8,6 +9,8 @@ const root = document.getElementById("root");
 if (!root) {
   throw new Error("Unable to find the application root element.");
 }
+
+
 
 createRoot(root).render(
   <StrictMode>
